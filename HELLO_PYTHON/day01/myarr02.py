97a29dc6d81425cbebc8fb3d4fc38aec09673642
Sungmin Park <107343662+SungminPark-FLOYD@users.noreply.github.com>
2024-02-21 14:00:53 +0900

    2024.02.21 / python 기본 문법

---
+++ HELLO_PYTHON/day01/myarr02.py
@@ -0,0 +1,7 @@
+# range = 함수형 배열 생성
+# list = 함수형 배열을 배열로 변환
+arr = list(range(3))
+print(arr)
+
+arr1 = list(range(30, 41))
+print(arr1)
---
+++ HELLO_PYTHON/day01/myarr03.py
@@ -0,0 +1,4 @@
+arr = ["홍길동", "전우치"]
+arr.append("이순신");
+arr.insert(len(arr), "철수")
+print(arr)
